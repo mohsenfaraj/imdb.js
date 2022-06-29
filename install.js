5fc47a94cad0335bbd34147fb8e218bef7d44ff0
Mohsen Farajollahi <gmohsenfarajollahi@gmail.com>
2022-06-29 13:13:35 +0430

    fixed tables

--- install.js
+++ install.js
@@ -2,25 +2,22 @@ module.exports = async function init(conn) {
     try {
         await conn.query(`CREATE TABLE IF NOT EXISTS User (
             ID INT NOT NULL AUTO_INCREMENT,
-            email VARCHAR(45) NOT NULL,
-            password VARCHAR(45) NOT NULL,
+            email VARCHAR(45) NOT NULL UNIQUE,
+            password VARCHAR(254) NOT NULL,
             birth DATE NULL,
             name VARCHAR(45) NOT NULL,
             address VARCHAR(45) NULL,
             avatar VARCHAR(45) ,
             banned TINYINT NOT NULL DEFAULT 0,
-            PRIMARY KEY (ID, email));`) ;
+            PRIMARY KEY (ID));`) ;
         await conn.query(`
         CREATE TABLE IF NOT EXISTS Admin (
             ID INT NOT NULL AUTO_INCREMENT,
-            email VARCHAR(45) NOT NULL,
-            password VARCHAR(45) NOT NULL,
-            birth DATE NULL,
+            email VARCHAR(45) NOT NULL UNIQUE,
+            password VARCHAR(254) NOT NULL,
             name VARCHAR(45) NOT NULL,
-            address VARCHAR(45) NULL,
             avatar VARCHAR(45),
-            permissions VARCHAR(45) NOT NULL DEFAULT 0,
-            PRIMARY KEY (ID, email))      
+            PRIMARY KEY (ID))      
         `) ;
         await conn.query(`
         CREATE TABLE IF NOT EXISTS Film (
@@ -49,78 +46,23 @@ module.exports = async function init(conn) {
             PRIMARY KEY (ID))
         `) ;
         await conn.query(`
-        CREATE TABLE IF NOT EXISTS Admin_ban_User (
-            Admin_ID INT NOT NULL,
-            Admin_email VARCHAR(45) NOT NULL,
-            User_ID INT NOT NULL,
-            User_email VARCHAR(45) NOT NULL,
-            PRIMARY KEY (Admin_ID, Admin_email, User_ID, User_email),
-            CONSTRAINT fk_Admin_has_User_Admin1
-              FOREIGN KEY (Admin_ID , Admin_email)
-              REFERENCES Admin (ID , email)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION,
-            CONSTRAINT fk_Admin_has_User_User1
-              FOREIGN KEY (User_ID , User_email)
-              REFERENCES User (ID , email)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION)       
-        `) ;
-        await conn.query(`
-        CREATE TABLE IF NOT EXISTS Admin_Manage_Movie (
-            Admin_ID INT NOT NULL,
-            Admin_email VARCHAR(45) NOT NULL,
-            Movie_ID INT NOT NULL,
-            PRIMARY KEY (Admin_ID, Admin_email, Movie_ID),
-              FOREIGN KEY (Admin_ID , Admin_email)
-              REFERENCES imdb.Admin (ID , email)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION,
-            CONSTRAINT fk_Admin_has_Movie_Movie1
-              FOREIGN KEY (Movie_ID)
-              REFERENCES Film (ID)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION)    
-        `) ;
-        await conn.query(`
         CREATE TABLE IF NOT EXISTS Comment (
             ID INT NOT NULL,
             User_ID INT NOT NULL,
+            Movie_ID INT NOT NULL,
             date DATE NOT NULL,
             text VARCHAR(245) NOT NULL,
             accepted TINYINT NOT NULL DEFAULT 0,
-            PRIMARY KEY (ID, User_ID),
-            INDEX fk_Comment_User_idx (User_ID ASC),
-            CONSTRAINT fk_Comment_User
+            PRIMARY KEY (ID, User_ID , Movie_ID),
               FOREIGN KEY (User_ID)
               REFERENCES User (ID)
               ON DELETE NO ACTION
-              ON UPDATE NO ACTION)    
-        `) ;
-        await conn.query(` 
-        CREATE TABLE IF NOT EXISTS Movie_Comments (
-            Comment_ID INT NOT NULL,
-            Comment_User_ID INT NOT NULL,
-            Movie_ID INT NOT NULL,
-            Approved_By INT NOT NULL,
-            Admin_email VARCHAR(45) NOT NULL,
-            PRIMARY KEY (Comment_ID, Comment_User_ID, Movie_ID),
-            CONSTRAINT fk_Comment_has_Movie_Comment1
-              FOREIGN KEY (Comment_ID , Comment_User_ID)
-              REFERENCES Comment (ID , User_ID)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION,
-            CONSTRAINT fk_Comment_has_Movie_Movie1
+              ON UPDATE NO ACTION , 
               FOREIGN KEY (Movie_ID)
               REFERENCES Film (ID)
               ON DELETE NO ACTION
-              ON UPDATE NO ACTION,
-            CONSTRAINT fk_Movie_Comments_Admin1
-              FOREIGN KEY (Approved_By , Admin_email)
-              REFERENCES Admin (ID , email)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION)
-          ENGINE = InnoDB  
+              ON UPDATE NO ACTION
+        )    
         `) ;
 
         await conn.query(`
@@ -130,12 +72,10 @@ module.exports = async function init(conn) {
             User_ID INT NOT NULL,
             Movie_ID INT NOT NULL,
             PRIMARY KEY (ID, User_ID, Movie_ID),
-            CONSTRAINT fk_Stars_User1
               FOREIGN KEY (User_ID)
               REFERENCES User (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION,
-            CONSTRAINT fk_Stars_Movie1
               FOREIGN KEY (Movie_ID)
               REFERENCES Film (ID)
               ON DELETE NO ACTION
@@ -150,12 +90,10 @@ module.exports = async function init(conn) {
             Movie_ID INT NOT NULL,
             Artists_ID INT NOT NULL,
             PRIMARY KEY (ID),
-            CONSTRAINT fk_Awards_Movie1
               FOREIGN KEY (Movie_ID)
               REFERENCES Film (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION,
-            CONSTRAINT fk_Awards_Artists1
               FOREIGN KEY (Artists_ID)
               REFERENCES Artists (ID)
               ON DELETE NO ACTION
@@ -166,36 +104,14 @@ module.exports = async function init(conn) {
             Movie_ID INT NOT NULL,
             Artists_ID INT NOT NULL,
             PRIMARY KEY (Movie_ID, Artists_ID),
-            INDEX fk_Movie_has_Artists_Artists1_idx (Artists_ID ASC) ,
-            INDEX fk_Movie_has_Artists_Movie1_idx (Movie_ID ASC) ,
-            CONSTRAINT fk_Movie_has_Artists_Movie1
               FOREIGN KEY (Movie_ID)
               REFERENCES Film (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION,
-            CONSTRAINT fk_Movie_has_Artists_Artists1
               FOREIGN KEY (Artists_ID)
               REFERENCES Artists (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION) 
-        `) ;
-        await conn.query(`
-        CREATE TABLE IF NOT EXISTS Admin_approves_Comment (
-            Admin_ID INT NOT NULL,
-            Admin_email VARCHAR(45) NOT NULL,
-            Comment_ID INT NOT NULL,
-            Comment_User_ID INT NOT NULL,
-            PRIMARY KEY (Admin_ID, Admin_email, Comment_ID, Comment_User_ID),
-            CONSTRAINT fk_Admin_has_Comment_Admin1
-              FOREIGN KEY (Admin_ID , Admin_email)
-              REFERENCES Admin (ID , email)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION,
-            CONSTRAINT fk_Admin_has_Comment_Comment1
-              FOREIGN KEY (Comment_ID , Comment_User_ID)
-              REFERENCES Comment (ID , User_ID)
-              ON DELETE NO ACTION
-              ON UPDATE NO ACTION)    
         `) ;
     } catch(err) {
         console.log(err)
