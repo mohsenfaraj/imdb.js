89c09a364279fcb28234ab43666b4c7b552cd696
Hamidreza Babaiee <hamidrezababaieeFX.TR@gmail.com>
2022-07-01 01:35:53 +0430

    getSearchQUERY FOR ARTISTS +

--- artists.js
+++ artists.js
@@ -13,8 +13,13 @@ router.get("/" , (req , res) => {
     // #get all x artist
     const p = Number.parseInt(req.query.p) || 0;
     const offset = p * 20 || 0 ; 
-    conn.query(`SELECT * FROM artists LIMIT 2 OFFSET ?`,[offset],(err,result,field)=>{
-        conn.query(`SELECT COUNT(ID) AS Count FROM artists`,(err2,result2)=>{
+    const name = req.query.CelebrityName
+    const role = req.query.CelebrityRole
+    const queryobj = getSearchQuery(name,role,offset)
+    const queryobj2= getSearchQuery2(name,role,offset)
+    //console.log(queryobj)
+    conn.query(queryobj.query,queryobj.items,(err,result,field)=>{
+        conn.query(queryobj2.query,queryobj2.items,(err2,result2)=>{
 
             if(err || err2){
                 console.log(err)
@@ -48,5 +53,63 @@ router.get("/:id" , (req , res) => {
         }
     })
 })
+
+
+
+function getSearchQuery(name,role, offset){
+    let query = "SELECT * FROM artists "
+    const items = [{item : ["name" , name]} , {item : ["role" , role]}] ; 
+
+    const result = items.filter((current) => {
+        return current.item[1]
+    })
+    let resultItems = [] ;
+    if (result.length > 0) {
+        query += "WHERE "
+        result.forEach(object => {
+          if (object.item[0] == "role") {
+                query += "role=? AND " ;
+                resultItems.push(object.item[1])
+            } else{
+                query += object.item[0] + " LIKE ? AND "
+                resultItems.push(object.item[1] + "%")
+            }
+        })
+        query += "1 " ;
+    }
+    query += "LIMIT 20 OFFSET ?"
+    resultItems.push(offset)
+
+    return {query : query , items : resultItems};
+}
+function getSearchQuery2(name,role, offset){
+    let query = "SELECT COUNT(ID) AS Count FROM artists "
+    const items = [{item : ["name" , name]} , {item : ["role" , role]}] ; 
+
+    const result = items.filter((current) => {
+        return current.item[1]
+    })
+    let resultItems = [] ;
+    if (result.length > 0) {
+        query += "WHERE "
+        result.forEach(object => {
+          if (object.item[0] == "role") {
+                query += "role=? AND " ;
+                resultItems.push(object.item[1])
+            } else{
+                query += object.item[0] + " LIKE ? AND "
+                resultItems.push(object.item[1] + "%")
+            }
+        })
+        query += "1 " ;
+    }
+    query += "LIMIT 20 OFFSET ?"
+    resultItems.push(offset)
+
+    return {query : query , items : resultItems};
+}
+
+
+
 
 module.exports = router 
