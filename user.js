6bf4ab954824c9ce8fd9899946931b5eed7a5e5d
Hamidreza Babaiee <hamidrezababaieeFX.TR@gmail.com>
2022-07-03 17:33:16 +0430

    create method for update query

--- user.js
+++ user.js
@@ -32,20 +32,42 @@ router.get("/userrate" , (req , res) => {
 
 router.put("/profile" , (req , res) => {
     // update user profile
-    const id = req.session.id
+    res.send('updated successfully')
+    const ID = 1//req.session.id
     const password = req.body.password
-    const birth = req.body.birth
-    const name = req.body.name
-    const address = req.query.address
+    const username = req.body.username
+    const name   = req.body.name
+    const email  = req.query.email
     const avatar = req.body.avatar
-    conn.query(`UPDATE user SET password=? , birth=? , name=? , address=? , avatar=? WHERE ID=?`,
-    [password,birth,name,address,avatar,id],(err , result , field)=>{
+    const queryobj=getUpdateQuery(email,username,name,avatar,ID)
+     conn.query(queryobj.query,queryobj.items,(err,result)=>{
         if(err){
             console.log(err)
         }else{
-            res.send('updated successfully')
+            res.send('updated...')
         }
-    })
+     })
+
 })
+
+function getUpdateQuery(email,username,name,avatar,ID){
+    let query = "UPDATE user SET "
+    const items = [{item : ["email" , email]} , {item : ["username" , username]} , {item : ["name" , name]} , {item : ["avatar" , avatar]} ] ; 
+
+    const result = items.filter((current) => {
+        return current.item[1]
+    })
+    let resultItems = [] ;
+    if (result.length > 0) {
+        result.forEach(object => {
+                query += object.item[0] + "=? "
+                resultItems.push(object.item[1])
+        })
+        query += "WHERE ID=?" ;
+        resultItems.push(ID)
+    }
+    
+    return {query : query , items : resultItems};
+}
 
 module.exports = router ;
