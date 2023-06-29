5a51274604be0a555b7f487052cc91a10fc23c6a
mohsen faraj <gmohsenfarajollahi@gmail.com>
2023-06-29 13:01:04 +0330

    update the install.js and package.json for install

--- index.js
+++ index.js
@@ -2,212 +2,243 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mysql = require("mysql2/promise");
-const install = require("./install")
-const admin = require("./admin")
-const video = require("./video")
-const artists = require("./artists")
-const user = require("./user")
-var bodyParser = require('body-parser');
-var multer = require('multer');
+const admin = require("./admin");
+const video = require("./video");
+const artists = require("./artists");
+const user = require("./user");
+var bodyParser = require("body-parser");
+var multer = require("multer");
 var upload = multer();
 var session = require("express-session");
 var MySqlSession = require("express-mysql-session")(session);
 
 async function run() {
-    const conn = await mysql.createConnection({
-        host: process.env.DBHOST,
-        user: process.env.DBUSER,
-        password: process.env.DBPASS,
-        database: process.env.DBNAME
+  const conn = await mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME,
+  });
+  var sessionStore = new MySqlSession({} /* session store options */, conn);
+  app.use(
+    session({
+      key: process.env.SESSION_NAME,
+      secret: process.env.SESSION_SECRET,
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
     })
-    install(conn);
-    var sessionStore = new MySqlSession({}/* session store options */, conn);
-    app.use(session({
-        key: process.env.SESSION_NAME,
-        secret: process.env.SESSION_SECRET,
-        store: sessionStore,
-        resave: false,
-        saveUninitialized: false
-    }));
-    app.set("view engine", "ejs");
-    app.use("/assets", express.static("assets"))
-    app.use("/public", express.static("public"))
+  );
+  app.set("view engine", "ejs");
+  app.use("/assets", express.static("assets"));
+  app.use("/public", express.static("public"));
 
-    // for parsing application/json
-    app.use(bodyParser.json());
+  // for parsing application/json
+  app.use(bodyParser.json());
 
-    // for parsing application/xwww-form-urlencoded
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.raw());
+  // for parsing application/xwww-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.raw());
 
-    // for parsing multipart/form-data
-    app.use(upload.array());
+  // for parsing multipart/form-data
+  app.use(upload.array());
 
-    app.use("/admin", [auth, admin])
+  app.use("/admin", [auth, admin]);
 
-    app.get("/", (req, res) => {
+  app.get("/", (req, res) => {
+    try {
+      // series , movie
+      (async () => {
+        const series = "Series";
+        const movie = "Movie";
+        const [result1] = await conn.query(
+          `SELECT name,ID,cover,genre,average FROM film WHERE average>5 LIMIT 12`
+        );
+        const [result2] = await conn.query(
+          `SELECT name,cover,ID,average FROM film WHERE type=? ORDER BY date_published DESC LIMIT 5 `,
+          [movie]
+        );
+        const [result3] = await conn.query(
+          `SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY average DESC LIMIT 5`,
+          [movie]
+        );
+        const [result4] = await conn.query(
+          `SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY comment_count DESC LIMIT 5`,
+          [movie]
+        );
+        const [result5] = await conn.query(
+          `SELECT name,cover,ID,average FROM film WHERE type=? ORDER BY date_published DESC LIMIT 5`,
+          [series]
+        );
+        const [result6] = await conn.query(
+          `SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY average DESC LIMIT 5`,
+          [series]
+        );
+        const [result7] = await conn.query(
+          `SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY comment_count DESC LIMIT 5`,
+          [series]
+        );
+        const [result8] = await conn.query(
+          `SELECT ID,name,role,avatar FROM artists ORDER BY award_count LIMIT 4`
+        );
+        res.render("index/main", {
+          user: req.session.user,
+          movie_slide: result1,
+          new_movies: result2,
+          top_movies: result3,
+          most_reviews_movies: result4,
+          new_series: result5,
+          top_series: result6,
+          most_reviews_series: result7,
+          spotlight_artists: result8,
+        });
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-        try {
-            // series , movie
-        (async () => {
-            const series = "Series"
-            const movie = "Movie"
-            const [result1] = await conn.query(`SELECT name,ID,cover,genre,average FROM film WHERE average>5 LIMIT 12`);
-            const [result2] = await conn.query(`SELECT name,cover,ID,average FROM film WHERE type=? ORDER BY date_published DESC LIMIT 5 `,[movie]) ;
-            const [result3] = await conn.query(`SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY average DESC LIMIT 5`,[movie]) ;
-            const [result4] = await conn.query(`SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY comment_count DESC LIMIT 5`,[movie]);
-            const [result5] = await conn.query(`SELECT name,cover,ID,average FROM film WHERE type=? ORDER BY date_published DESC LIMIT 5`,[series]) ;
-            const [result6] = await conn.query(`SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY average DESC LIMIT 5`,[series]) ;
-            const [result7] = await conn.query(`SELECT name,cover,ID,average FROM film  WHERE type=? ORDER BY comment_count DESC LIMIT 5`,[series]);
-            const [result8] = await conn.query(`SELECT ID,name,role,avatar FROM artists ORDER BY award_count LIMIT 4`);
-            res.render("index/main", {
-                user: req.session.user,
-                movie_slide: result1,
-                new_movies: result2,
-                top_movies: result3,
-                most_reviews_movies:result4,
-                new_series:result5,
-                top_series:result6,
-                most_reviews_series:result7,
-                spotlight_artists:result8
-            });
-        })();
-        }catch(error) {
-            console.log(error)
+  app.use("/video", video);
+  app.use("/artist", artists);
+  app.use("/user", [userAuth, user]);
+
+  app.get("/login", (req, res) => {
+    if (req.session.Admin) {
+      res.redirect("/admin");
+    } else {
+      res.render("admin/login");
+    }
+  });
+
+  app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("back");
+  });
+
+  app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    conn
+      .query("SELECT * FROM admin WHERE email = ? AND password = ?", [
+        email,
+        password,
+      ])
+      .then(([result]) => {
+        if (result.length > 0) {
+          req.session.Admin = {
+            id: result[0].ID,
+            name: result[0].name,
+            email: result[0].email,
+          };
+          res.redirect("/admin");
+        } else {
+          res.render("regMessage", {
+            headertext: "",
+            message: "the username or password is invalid.",
+          });
         }
-    })
+      })
+      .catch((err) => {
+        res.render("regMessage", {
+          headertext: "there was an error",
+          message: "there was an error",
+        });
+        console.log(err);
+      });
+  });
 
-    app.use("/video", video);
-    app.use("/artist", artists);
-    app.use("/user",  [userAuth, user]);
-
-    app.get("/login", (req, res) => {
-        if (req.session.Admin) {
-            res.redirect("/admin")
+  app.post("/userLogin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    conn
+      .query("SELECT * FROM user WHERE username = ? AND password = ?", [
+        username,
+        password,
+      ])
+      .then(([result]) => {
+        if (result.length > 0) {
+          req.session.user = {
+            name: result[0].name || username,
+            id: result[0].ID,
+            banned: result[0].banned,
+            avatar: result[0].avatar,
+          };
+          res.redirect("back");
+        } else {
+          res.render("regMessage", {
+            headertext: "",
+            message: "the username or password is invalid.",
+          });
         }
-        else {
-            res.render("admin/login")
-        }
-    })
+      })
+      .catch((err) => {
+        res.render("regMessage", {
+          headertext: "there was an error",
+          message: "there was an error",
+        });
+        console.log(err);
+      });
+  });
 
-    app.get("/logout", (req, res) => {
-        req.session.destroy();
-        res.redirect("back");
-    })
+  app.post("/register", (req, res) => {
+    const name = req.body.username;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    app.post("/login", (req, res) => {
-        const email = req.body.email;
-        const password = req.body.password;
-        conn.query("SELECT * FROM admin WHERE email = ? AND password = ?", [email, password])
-            .then(([result]) => {
-                if (result.length > 0) {
-                    req.session.Admin = {
-                        id : result[0].ID ,
-                        name : result[0].name ,
-                        email : result[0].email
+    if (!name || !email || !password || !username) {
+      res.render("regMessage", {
+        headertext: "fill all the fields",
+        message: "please fill all the required fields",
+      });
+      return;
+    }
+    //TODO: hash the password
 
-                    };
-                    res.redirect("/admin")
-                }
-                else {
-                    res.render("regMessage", {
-                        headertext: "",
-                        message: "the username or password is invalid."
-                    })
-                }
-            }).catch((err) => {
-                res.render("regMessage", {
-                    headertext: "there was an error",
-                    message: "there was an error"
-                })
-                console.log(err);
-            })
-    });
-
-    app.post("/userLogin", (req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-        conn.query("SELECT * FROM user WHERE username = ? AND password = ?", [username, password])
-            .then(([result]) => {
-                if (result.length > 0) {
-                    req.session.user = {
-                        name: result[0].name || username,
-                        id: result[0].ID,
-                        banned: result[0].banned,
-                        avatar: result[0].avatar
-                    }
-                    res.redirect("back")
-                }
-                else {
-                    res.render("regMessage", {
-                        headertext: "",
-                        message: "the username or password is invalid."
-                    })
-                }
-            }).catch((err) => {
-                res.render("regMessage", {
-                    headertext: "there was an error",
-                    message: "there was an error"
-                })
-                console.log(err);
-            })
-    })
-
-    app.post("/register", (req, res) => {
-        const name = req.body.username;
-        const email = req.body.email;
-        const username = req.body.username;
-        const password = req.body.password;
-
-        if (!name || !email || !password || !username) {
-            res.render("regMessage", {
-                headertext: "fill all the fields",
-                message: "please fill all the required fields"
-            })
-            return ;
-        }
-        //TODO: hash the password
-
-        //TODO: generate a gravatar image link
-        conn.query("INSERT INTO `user` (`email`, `password`, `name` , `username`) VALUES (? , ? , ? , ?)",
-            [email, password, name, username]).then(([row, fields]) => {
-                res.render("regMessage", {
-                    headertext: "Account Created!",
-                    message: "the creation of account was successful!"
-                })
-            }).catch((err) => {
-                res.render("regMessage", {
-                    headertext: "Account Already Exists",
-                    message: "there was a problem in creating your account. check if acount already exists."
-                });
-                console.log(err)
-            })
-    })
-    app.use((req,res,next)=> {
-        res.status(404).render("index/page404")
-    })
-    app.listen(3000, () => {
-        console.log("server is running on port 3000")
-    })
+    //TODO: generate a gravatar image link
+    conn
+      .query(
+        "INSERT INTO `user` (`email`, `password`, `name` , `username`) VALUES (? , ? , ? , ?)",
+        [email, password, name, username]
+      )
+      .then(([row, fields]) => {
+        res.render("regMessage", {
+          headertext: "Account Created!",
+          message: "the creation of account was successful!",
+        });
+      })
+      .catch((err) => {
+        res.render("regMessage", {
+          headertext: "Account Already Exists",
+          message:
+            "there was a problem in creating your account. check if acount already exists.",
+        });
+        console.log(err);
+      });
+  });
+  app.use((req, res, next) => {
+    res.status(404).render("index/page404");
+  });
+  app.listen(3000, () => {
+    console.log("server is running on port 3000");
+  });
 }
 
 // authenticate user
 function auth(req, res, next) {
-    if (req.session.Admin) {
-        next();
-    }
-    else {
-        res.redirect("/login")
-    }
+  if (req.session.Admin) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
 }
 
- function userAuth(req,res,next){
-    if(req.session.user){
-        next();
-    }else{
-        res.redirect("/")
-    }
- }
+function userAuth(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+}
 
 run();
-
--- install.js
+++ install.js
@@ -1,6 +1,14 @@
-module.exports = async function init(conn) {
-    try {
-        await conn.query(`CREATE TABLE IF NOT EXISTS User (
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+async function init() {
+  const conn = await mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME,
+  });
+  try {
+    await conn.query(`CREATE TABLE IF NOT EXISTS user (
             ID INT NOT NULL AUTO_INCREMENT,
             email VARCHAR(45) NOT NULL UNIQUE,
             password VARCHAR(254) NOT NULL,
@@ -8,18 +16,18 @@ module.exports = async function init(conn) {
             name VARCHAR(45) NOT NULL,
             avatar VARCHAR(45) ,
             banned TINYINT NOT NULL DEFAULT 0,
-            PRIMARY KEY (ID));`) ;
-        await conn.query(`
-        CREATE TABLE IF NOT EXISTS Admin (
+            PRIMARY KEY (ID));`);
+    await conn.query(`
+        CREATE TABLE IF NOT EXISTS admin (
             ID INT NOT NULL AUTO_INCREMENT,
             email VARCHAR(45) NOT NULL UNIQUE,
             password VARCHAR(254) NOT NULL,
             name VARCHAR(45) NOT NULL,
             avatar VARCHAR(45),
             PRIMARY KEY (ID))      
-        `) ;
-        await conn.query(`
-        CREATE TABLE IF NOT EXISTS Film (
+        `);
+    await conn.query(`
+        CREATE TABLE IF NOT EXISTS film (
             ID INT NOT NULL AUTO_INCREMENT,
             type VARCHAR(45) NULL,
             name VARCHAR(45) NOT NULL,
@@ -34,9 +42,9 @@ module.exports = async function init(conn) {
             average FLOAT NOT NULL DEFAULT 0 ,
             comment_count INT NOT NULL DEFAULT 0,
             PRIMARY KEY (ID))   
-        `) ;
-        await conn.query(` 
-        CREATE TABLE IF NOT EXISTS Artists (
+        `);
+    await conn.query(` 
+        CREATE TABLE IF NOT EXISTS artists (
             ID INT NOT NULL AUTO_INCREMENT,
             name VARCHAR(45) NOT NULL,
             avatar VARCHAR(255) NULL,
@@ -44,9 +52,9 @@ module.exports = async function init(conn) {
             role VARCHAR(45) NULL,
             award_count INT NOT NULL DEFAULT 0,
             PRIMARY KEY (ID))
-        `) ;
-        await conn.query(`
-        CREATE TABLE IF NOT EXISTS Comment (
+        `);
+    await conn.query(`
+        CREATE TABLE IF NOT EXISTS comment (
             ID INT NOT NULL AUTO_INCREMENT,
             User_ID INT NOT NULL,
             Movie_ID INT NOT NULL,
@@ -55,34 +63,34 @@ module.exports = async function init(conn) {
             accepted TINYINT NOT NULL DEFAULT 9,
             PRIMARY KEY (ID, User_ID , Movie_ID),
               FOREIGN KEY (User_ID)
-              REFERENCES User (ID)
+              REFERENCES user (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION , 
               FOREIGN KEY (Movie_ID)
-              REFERENCES Film (ID)
+              REFERENCES film (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION
         )    
-        `) ;
+        `);
 
-        await conn.query(`
-        CREATE TABLE IF NOT EXISTS Stars (
+    await conn.query(`
+        CREATE TABLE IF NOT EXISTS stars (
             ID INT NOT NULL AUTO_INCREMENT,
             Rating INT NOT NULL,
             User_ID INT NOT NULL,
             Movie_ID INT NOT NULL,
             PRIMARY KEY (ID, User_ID, Movie_ID),
               FOREIGN KEY (User_ID)
-              REFERENCES User (ID)
+              REFERENCES user (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION,
               FOREIGN KEY (Movie_ID)
-              REFERENCES Film (ID)
+              REFERENCES film (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION)   
-        `) ;
-        await conn.query(` 
-        CREATE TABLE IF NOT EXISTS Awards (
+        `);
+    await conn.query(` 
+        CREATE TABLE IF NOT EXISTS awards (
             ID INT NOT NULL AUTO_INCREMENT,
             Name VARCHAR(45) NOT NULL,
             Date DATE NULL,
@@ -91,63 +99,67 @@ module.exports = async function init(conn) {
             Artists_ID INT NULL,
             PRIMARY KEY (ID),
               FOREIGN KEY (Movie_ID)
-              REFERENCES Film (ID)
+              REFERENCES film (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION,
               FOREIGN KEY (Artists_ID)
-              REFERENCES Artists (ID)
+              REFERENCES artists (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION)  
-        `) ;
-        await conn.query(` 
+        `);
+    await conn.query(` 
         CREATE TABLE IF NOT EXISTS Movie_has_Artists (
             Movie_ID INT NOT NULL,
             Artists_ID INT NOT NULL,
             Description VARCHAR(45) NULL,
             PRIMARY KEY (Movie_ID, Artists_ID),
               FOREIGN KEY (Movie_ID)
-              REFERENCES Film (ID)
+              REFERENCES film (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION,
               FOREIGN KEY (Artists_ID)
-              REFERENCES Artists (ID)
+              REFERENCES artists (ID)
               ON DELETE NO ACTION
               ON UPDATE NO ACTION) 
-        `) ;
-        await conn.query(`
+        `);
+    await conn.query(`
         CREATE TRIGGER IF NOT EXISTS Update_filme_rating AFTER INSERT ON stars FOR
         EACH ROW UPDATE film SET average=(SELECT AVG(Rating) AS AVERAGE FROM stars 
         WHERE film.ID=stars.Movie_ID)
-        `)
-        await conn.query(`
+        `);
+    await conn.query(`
         CREATE TRIGGER IF NOT EXISTS Update_filme_rating2 AFTER UPDATE ON stars FOR
         EACH ROW UPDATE film SET average=(SELECT AVG(Rating) AS AVERAGE FROM stars 
         WHERE film.ID=stars.Movie_ID)
-        `)
-        await conn.query(`
+        `);
+    await conn.query(`
         CREATE TRIGGER IF NOT EXISTS Update_filme_rating3 AFTER DELETE ON stars FOR
         EACH ROW UPDATE film SET average=(SELECT AVG(Rating) AS AVERAGE FROM stars 
         WHERE film.ID=stars.Movie_ID)
-        `)
-        await conn.query(`CREATE TRIGGER  IF NOT EXISTS COMMENT_COUNT AFTER UPDATE 
+        `);
+    await conn.query(`CREATE TRIGGER  IF NOT EXISTS COMMENT_COUNT AFTER UPDATE 
         ON comment FOR EACH ROW UPDATE film SET comment_count=(SELECT COUNT(comment
         .Movie_ID) FROM comment WHERE film.ID=comment.Movie_ID AND accepted=1)
-        `)
-        await conn.query(`CREATE TRIGGER  IF NOT EXISTS COMMENT_COUNT1 AFTER DELETE 
+        `);
+    await conn.query(`CREATE TRIGGER  IF NOT EXISTS COMMENT_COUNT1 AFTER DELETE 
         ON comment FOR EACH ROW UPDATE film SET comment_count=(SELECT COUNT(comment
         .Movie_ID) FROM comment WHERE film.ID=comment.Movie_ID AND accepted=1)
-        `)
-        await conn.query(`CREATE TRIGGER  IF NOT EXISTS AWARD_COUNT AFTER UPDATE 
+        `);
+    await conn.query(`CREATE TRIGGER  IF NOT EXISTS AWARD_COUNT AFTER UPDATE 
         ON awards FOR EACH ROW UPDATE artists SET  award_count=(SELECT COUNT(awards
         .Artists_ID) FROM awards WHERE artists.ID=awards.Artists_ID)
-        `)
-        await conn.query(`CREATE TRIGGER  IF NOT EXISTS AWARD_COUNT1 AFTER DELETE 
+        `);
+    await conn.query(`CREATE TRIGGER  IF NOT EXISTS AWARD_COUNT1 AFTER DELETE 
         ON awards FOR EACH ROW UPDATE artists SET award_count=(SELECT COUNT(awards
         .Artists_ID) FROM awards WHERE artists.ID=awards.Artists_ID)
-        `)
-    } catch(err) {
-        console.log(err)
-    }
-
-    
+        `);
+    await conn.query(
+      "INSERT IGNORE INTO `admin` (`ID`, `email`, `password`, `name`, `avatar`) VALUES (1, 'admin@example.com', 'password', 'admin', NULL);"
+    );
+    conn.end();
+  } catch (err) {
+    console.log(err);
+    conn.end();
+  }
 }
+init();
